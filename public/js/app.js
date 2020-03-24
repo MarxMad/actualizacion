266dfb4866b45ca8e7cbc4995f0341684f5c05e8
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2020-03-24 12:24:27 -0600

    marzo 24

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,15 @@
 M.AutoInit();
 var instance = M.Tabs.init(el, options);
 $('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
+  var $this = $(this);
+  var $target = $('#' + $(this).attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
   });
+});
 var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
-  });
+  fullWidth: true,
+  indicators: true
+});
+
